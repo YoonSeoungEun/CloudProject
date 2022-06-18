27cb4ffdab0ef1f7b236ec31028fb1819b598b38
Seoungeun Yoon <yo6689@naver.com>
2022-06-18 20:57:21 +0900

    static, templates, urls.py commit

---
+++ movie/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path, include
+
+from . import views
+
+urlpatterns = [
+    path('', views.PostList.as_view()),
+    path('<int:pk>/', views.PostDetail.as_view()),
+    path('category/<str:slug>/', views.show_category_posts),
+    path('tag/<str:slug>/', views.show_tag_posts),
+]
